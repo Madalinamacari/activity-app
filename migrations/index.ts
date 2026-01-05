972298f74d5b34b091bc15533f7dec1844608a98
Madalinamacari <madalinainfo24@gmail.com>
2026-01-05 17:07:54 +0200

    Add files via upload

---
+++ migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260104_002414 from './20260104_002414';
+
+export const migrations = [
+  {
+    up: migration_20260104_002414.up,
+    down: migration_20260104_002414.down,
+    name: '20260104_002414'
+  },
+];
